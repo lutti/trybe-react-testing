c3359c2a21602919e82a97323414fe9da289ea52
Lutti <lutti.c.amaral@gmail.com>
2022-08-16 16:18:15 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('É exibido na tela um h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const pageReq = screen.getByRole(
+      'heading',
+      { name: /Page requested not found/, level: 2 },
+    );
+    expect(pageReq).toBeInTheDocument();
+  });
+
+  test('Existe uma imagem com o src https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const imagem = screen.getByAltText(/Pikachu crying/i);
+    expect(imagem.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
