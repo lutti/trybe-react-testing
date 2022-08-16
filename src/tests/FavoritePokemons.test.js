28c3b03105a83cf4ec70a818074d9810fc56361c
Lutti <lutti.c.amaral@gmail.com>
2022-08-16 16:00:37 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<App />);
+
+    const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(linkFavorite);
+
+    const noPokemon = screen.getByText('No favorite pokemon found');
+    expect(noPokemon).toBeInTheDocument();
+  });
+});
